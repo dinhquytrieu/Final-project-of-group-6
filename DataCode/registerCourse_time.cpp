2222fc33cdb622d4f3080d0fcba1b2a9e66e3a85
dinhquytrieu <101011727+dinhquytrieu@users.noreply.github.com>
2022-03-20 10:24:03 +0700

    time of registration completed

--- DataCode/registerCourse_time.cpp
+++ DataCode/registerCourse_time.cpp
@@ -10,11 +10,24 @@ void register_course(semester *&semesterNew, ofstream &fileout){
     cout << " day: ";
     cin >> semesterNew->regStart.day;
 
+    cout << "\t\tEnter end date of registration of the course: " << endl;
+    cout << "YEAR: ";
+    cin >> semesterNew->regEnd.year;
+    cout << " month: ";
+    cin >> semesterNew->regEnd.month;
+    cout << " day: ";
+    cin >> semesterNew->regEnd.day;
+
+
+
     fileout.open("C:\\GitHub\\Final-project-of-group-6\\datafile\\time_registration.txt", ios_base::out);
  //   fileout << endl;
     fileout << semesterNew->regStart.year << " ";
     fileout << semesterNew->regStart.month << " ";
     fileout << semesterNew->regStart.day << " ";
 
+    fileout << semesterNew->regEnd.year << " ";
+    fileout << semesterNew->regEnd.month << " ";
+    fileout << semesterNew->regEnd.day << " ";
     fileout.close();
 }
