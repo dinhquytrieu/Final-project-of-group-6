e75b071333ba40e107e27c2ffc2754b3915b86e3
phanthaikhang21127069 <ptkhang21@clc.fitus.edu.vn>
2022-03-20 11:38:02 +0700

    Create course.h

---
+++ DataCode/course.h
@@ -0,0 +1,29 @@
+#ifndef _COURSE_H_
+#define _COURSE_H_
+
+#include"header.h"
+
+// Create year
+
+
+
+// Create Semester
+
+
+// Create Course registration session
+
+
+// Create Course
+
+
+//View list of all course available
+
+
+// Delete Course
+
+deleteCourse(Year * pYear, course *&courseHead, string IDcourse);
+
+// Update Course
+
+#endif
+
