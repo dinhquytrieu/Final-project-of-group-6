8fe9b35ee6940290c209cb6d605bac480c510cb5
dinhquytrieu <101011727+dinhquytrieu@users.noreply.github.com>
2022-03-21 22:54:35 +0700

    deleting course is ok

--- DataCode/delete_course.cpp
+++ DataCode/delete_course.cpp
@@ -2,38 +2,38 @@
 #include"course.h"
 
 using namespace std;
-//abc
-void deleteCourse(Year * pYear, course *&courseHead, string IDcourse) {
-	while(1) {
-		cout << "Are you sure you want to delete this course?\n\n";
-		cout << "0: No\n";
-		cout << "1: Yes\n\n";
 
-   		cout << "Answer: ";
-   		char *respond = new char[101]; 
-		cin >> respond;
-   		system("cls");
-   		if (strlen(respond) > 1 || (respond[0] < '0' || '1' < respond[0])) {
-   			cout << "Invalid, please try again\n\n";
-   			continue;
-   		}
-   		int x = respond[0] - '0';
-   		delete[] respond;
-   		if (!x)
-   			return;
-   		break;		
-	}
+void deleteCourse(course *&courseHead, string IDcourse) {
+	// while(1) {
+	// 	cout << "Are you sure you want to delete this course?\n\n";
+	// 	cout << "0: No\n";
+	// 	cout << "1: Yes\n\n";
+
+   	// 	cout << "Answer: ";
+   	// 	char *respond = new char[101]; 
+	// 	cin >> respond;
+   	// 	system("cls");
+   	// 	if (strlen(respond) > 1 || (respond[0] < '0' || '1' < respond[0])) {
+   	// 		cout << "Invalid, please try again\n\n";
+   	// 		continue;
+   	// 	}
+   	// 	int x = respond[0] - '0';
+   	// 	delete[] respond;
+   	// 	if (!x)
+   	// 		return;
+   	// 	break;		
+	// }
  	// Then we simply delete this course
 
-    if (strcmp(courseHead->IDcourse, IDcourse) == 0) {
+    if (strcmp(courseHead->IDcourse.c_str(), IDcourse.c_str()) == 0) {
         course* pDel = courseHead;
         courseHead = courseHead -> pCourseNext;
         delete pDel;
     }
     else {
         course* pCur = courseHead;
-        while (pCur -> pCourseNext && strcmp(pCur -> pCourseNext -> IDcourse, IDcourse) != 0) {
-            pCur = pCur -> courseNext;
+        while (pCur -> pCourseNext && strcmp(pCur -> pCourseNext -> IDcourse.c_str(), IDcourse.c_str()) != 0) {
+            pCur = pCur -> pCourseNext;
         }
         if (pCur -> pCourseNext) {
             course* pDel = pCur -> pCourseNext;
@@ -51,8 +51,8 @@ void deleteCourse(Year * pYear, course *&courseHead, string IDcourse) {
         fout << pCur -> teacherName << '\n';
         fout << pCur -> sumCredit << '\n';
         fout << pCur -> maxStudent << '\n';
-        fout << pCur -> date.d1 << '\n' << pCur->date.TIME1 << '\n';
-	    fout << pCur -> date.d2 << '\n' << pCur->date.TIME2 << '\n';
+        fout << pCur -> performDate.d1 << '\n' << pCur->performDate.TIME1.hour << endl <<  pCur->performDate.TIME1.min << '\n';
+	    fout << pCur -> performDate.d2 << '\n' <<  pCur->performDate.TIME2.hour << endl <<  pCur->performDate.TIME2.min <<'\n';
     	pCur = pCur -> pCourseNext;
  	}
  	fout.close();
