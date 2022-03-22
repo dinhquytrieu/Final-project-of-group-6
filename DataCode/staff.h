0bc2e64c786adffce29dc5ca5796d16e145658d5
phanthaikhang21127069 <ptkhang21@clc.fitus.edu.vn>
2022-03-22 11:41:36 +0700

    Create staff.h

---
+++ DataCode/staff.h
@@ -0,0 +1,20 @@
+#ifndef _STAFF_H_
+#define _STAFF_H_
+
+#include "header.h"
+
+//void viewListOfCLasses(Year* pYear);
+
+//void viewStudentInfo(Student student);
+//void viewListOfStudentsInClass(Year* pYear, char* className);
+//void viewScoreBoardOfClass(Student * pStudent, Semester * pSemester);
+
+void viewListOfStudentsInCourse(Student *pStu, char *courseID);
+//void viewScoreboardOfCourse (Course *pCourse);
+//
+//void inputScoreboardCSV(Scoreboard *& newScr , char *s);
+//void addScoreBoardCSV(Scoreboard *& pScore, char * yearName, char * semesterName, char * courseName);
+//void exportStudentToCsv(Student* pStuInCourse, char *courseID);
+//void updateScoreBoardStudent(Scoreboard *& pScr, char *yearName, char* semesterName, char *courseID, char* studentID);
+
+#endif
