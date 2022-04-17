dc7a9830084e7b6e52e18ad1870681a08c38ed2e
dinhquytrieu <101011727+dinhquytrieu@users.noreply.github.com>
2022-04-17 17:31:32 +0700

    Update registerCourse_time.cpp

--- DataCode/registerCourse_time.cpp
+++ DataCode/registerCourse_time.cpp
@@ -1,33 +1,46 @@
 #include "header.h"
 
-void register_course(semester *&semesterNew, ofstream &fileout){
-   
-    cout << "\t\tEnter start date of registration of the course: " << endl;
-    cout << "YEAR: ";
-    cin >> semesterNew->regStart.year;
-    cout << " month: ";
-    cin >> semesterNew->regStart.month;
-    cout << " day: ";
-    cin >> semesterNew->regStart.day;
+void courseRegistration(Semester *& pSemester, Semester * totSemester, char * yearName) {
 
-    cout << "\t\tEnter end date of registration of the course: " << endl;
-    cout << "YEAR: ";
-    cin >> semesterNew->regEnd.year;
-    cout << " month: ";
-    cin >> semesterNew->regEnd.month;
-    cout << " day: ";
-    cin >> semesterNew->regEnd.day;
+	textColor(14);
+	cout << "Start Date: \n";
+	textColor(7);
+	cout << "Day: "; cin >> pSemester -> startReg.day;
+	cout << "Month: "; cin >> pSemester -> startReg.month;
+	cout << "Year: "; cin >> pSemester -> startReg.year;
+	
+	
+	textColor(14);
+	cout << "End date: \n";	
+	textColor(7);
+	cout << "Day: "; cin >> pSemester -> endReg.day;
+	cout << "Month: "; cin >> pSemester -> endReg.month;
+	cout << "Year: "; cin >> pSemester -> endReg.year;
+	cout<<endl;
+	textColor(10);
+	cout << "The registration session for this semester is from " << pSemester -> startReg.day << '/' << pSemester -> startReg.month << '/' << pSemester -> startReg.year << " to "
+			<< pSemester -> endReg.day << '/' << pSemester -> endReg.month << '/' << pSemester -> endReg.year << '\n';
+			textColor(7);
+	system("pause");
+	system("cls");
 
+	char dirD[] = "C:\\Github\\Final-project-of-group-6\\DataProject\\";
+	char c[500] = "";
+	strcat(c, dirD);
+	strcat(c, yearName);
+	strcat(c, "\\Semester");
+	strcat(c, "\\Semester.txt");
 
+	ofstream fOut(c);
 
-    fileout.open("C:\\GitHub\\Final-project-of-group-6\\datafile\\time_registration.txt", ios_base::out);
- //   fileout << endl;
-    fileout << semesterNew->regStart.year << " ";
-    fileout << semesterNew->regStart.month << " ";
-    fileout << semesterNew->regStart.day << " ";
-
-    fileout << semesterNew->regEnd.year << " ";
-    fileout << semesterNew->regEnd.month << " ";
-    fileout << semesterNew->regEnd.day << " ";
-    fileout.close();
+	Semester * cur = totSemester;
+	while (cur != nullptr) {
+		fOut << cur -> SemesterName << ' ';
+		fOut << cur -> startDate.year << ' ' << cur -> startDate.month << ' ' << cur -> startDate.day << ' ';
+		fOut << cur -> endDate.year << ' ' << cur -> endDate.month << ' ' << cur -> endDate.day << ' ';
+		fOut << cur -> startReg.year << ' ' << cur -> startReg.month << ' ' << cur -> startReg.day << ' ';
+		fOut << cur -> endReg.year << ' ' << cur -> endReg.month << ' ' << cur -> endReg.day << '\n';
+		cur = cur -> semesterNext;
+	}
+	fOut.close();
 }
