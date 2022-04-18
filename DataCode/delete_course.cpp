f762a0bd17c417b73571c87b00aaaa52c1e69321
dinhquytrieu <101011727+dinhquytrieu@users.noreply.github.com>
2022-04-18 18:38:29 +0700

    Update delete_course.cpp

--- DataCode/delete_course.cpp
+++ DataCode/delete_course.cpp
@@ -1,60 +1,41 @@
-#include"header.h"
-#include"course.h"
-
-using namespace std;
-
-void deleteCourse(course *&courseHead, string IDcourse) {
-	// while(1) {
-	// 	cout << "Are you sure you want to delete this course?\n\n";
-	// 	cout << "0: No\n";
-	// 	cout << "1: Yes\n\n";
-
-   	// 	cout << "Answer: ";
-   	// 	char *respond = new char[101]; 
-	// 	cin >> respond;
-   	// 	system("cls");
-   	// 	if (strlen(respond) > 1 || (respond[0] < '0' || '1' < respond[0])) {
-   	// 		cout << "Invalid, please try again\n\n";
-   	// 		continue;
-   	// 	}
-   	// 	int x = respond[0] - '0';
-   	// 	delete[] respond;
-   	// 	if (!x)
-   	// 		return;
-   	// 	break;		
-	// }
- 	// Then we simply delete this course
-
-    if (strcmp(courseHead->IDcourse.c_str(), IDcourse.c_str()) == 0) {
-        course* pDel = courseHead;
-        courseHead = courseHead -> pCourseNext;
+void deleteCourse(Year * pYear, Course*& pCourse, char* semesterName, char* courseID) {
+	
+  if (strcmp(pCourse->id, courseID) == 0) {
+        Course* pDel = pCourse;
+        pCourse = pCourse->courseNext;
         delete pDel;
     }
     else {
-        course* pCur = courseHead;
-        while (pCur -> pCourseNext && strcmp(pCur -> pCourseNext -> IDcourse.c_str(), IDcourse.c_str()) != 0) {
-            pCur = pCur -> pCourseNext;
+        Course* pCur = pCourse;
+        while (pCur->courseNext && strcmp(pCur->courseNext->id, courseID) != 0) {
+            pCur = pCur -> courseNext;
         }
-        if (pCur -> pCourseNext) {
-            course* pDel = pCur -> pCourseNext;
-            pCur -> pCourseNext = pDel -> pCourseNext;
+        if (pCur->courseNext) {
+            Course* pDel = pCur->courseNext;
+            pCur->courseNext = pDel->courseNext;
             delete pDel;
         }
     }
-    
-    ofstream fout("C:\\Github\\Final-project-of-group-6\\datafile\\description_courses.txt");
+	char path[] = "C:\\Github\\Final-project-of-group-6\\DataProject\\";
+    char c[500] = "";
+    strcat(c, path);
+    strcat(c, yearName);
+    strcat(c, "\\Semester\\");
+    strcat(c, semesterName);
+    strcat(c, "\\Course.txt");
 
-    course * pCur = courseHead;
-    while (pCur != NULL) {
-        fout << pCur -> IDcourse << '\n';
-        fout << pCur -> NAMEcourse << '\n';
-        fout << pCur -> teacherName << '\n';
-        fout << pCur -> sumCredit << '\n';
+    ofstream fout(c);
+
+    Course * pCur = pCourse;
+    while (pCur != nullptr) {
+        fout << pCur -> id << '\n';
+        fout << pCur -> name << '\n';
+        fout << pCur -> lecturerName << '\n';
+        fout << pCur -> numberOfCredits << '\n';
         fout << pCur -> maxStudent << '\n';
-        fout << pCur -> performDate.d1 << '\n' << pCur->performDate.TIME1.hour << endl <<  pCur->performDate.TIME1.min << '\n';
-	    fout << pCur -> performDate.d2 << '\n' <<  pCur->performDate.TIME2.hour << endl <<  pCur->performDate.TIME2.min <<'\n';
-    	pCur = pCur -> pCourseNext;
+        fout << pCur -> date.d1 << '\n' << pCur->date.s1 << '\n';
+	    fout << pCur -> date.d2 << '\n' << pCur->date.s2 << '\n';
+    	pCur = pCur -> courseNext;
  	}
  	fout.close();
 }
-
